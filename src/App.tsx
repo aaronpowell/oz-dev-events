8c66127a9edb9724841a5ef4fb7e67f16adb6a07
Aaron Powell <me@aaron-powell.com>
2019-03-21 15:53:17 +1100

    updating build and more years

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import downloadYear, { Year } from './fetch-events';
 import Table from './components/Table';
 import Loader from './components/Loader';
+import sortBy from 'lodash.sortby';
 
 interface AppState {
     years: Year[],
@@ -33,6 +34,22 @@ class App extends React.Component<{}, AppState> {
                     loading: false
                 })
             );
+
+        downloadYear('2017', 'https://raw.githubusercontent.com/Readify/DevEvents/master/2018.md')
+            .then(year =>
+                this.setState({
+                    years: [...this.state.years, year],
+                    loading: false
+                })
+            );
+
+        downloadYear('2016', 'https://raw.githubusercontent.com/Readify/DevEvents/master/2018.md')
+            .then(year =>
+                this.setState({
+                    years: [...this.state.years, year],
+                    loading: false
+                })
+            );
     }
 
     render() {
@@ -40,7 +57,7 @@ class App extends React.Component<{}, AppState> {
             <div>
                 {this.state.loading ?
                     <Loader /> :
-                    this.state.years.map(year => <Table year={year} key={year.year} />)}
+                    sortBy(this.state.years, 'year').reverse().map(year => <Table year={year} key={year.year} />)}
             </div>
         );
     }
