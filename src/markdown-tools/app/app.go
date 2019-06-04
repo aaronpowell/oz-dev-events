f5bc6ab8c15efeb5b3ebd9ebec822e97b54b6cc2
Aaron Powell <me@aaron-powell.com>
2019-06-04 10:49:35 +1000

    some tweaking of the 'server' component

--- src/markdown-tools/app/app.go
+++ src/markdown-tools/app/app.go
@@ -4,19 +4,28 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"fmt"
+	"github.com/jpillora/opts"
 
 	markdowntools ".."
 )
 
+type cli struct {
+	Year  string `opts:"help=Year to load,default=readme"`
+}
+
 func main() {
-	resp, err := http.Get("https://raw.githubusercontent.com/Readify/DevEvents/master/2018.md")
+	c := cli{Year: "readme"}
+	opts.Parse(&c)
+
+	resp, err := http.Get(fmt.Sprintf("https://raw.githubusercontent.com/Readify/DevEvents/master/%s.md", c.Year))
 	if err != nil {
 		println(err)
 	} else {
 		defer resp.Body.Close()
 		body, _ := ioutil.ReadAll(resp.Body)
 		year := markdowntools.ParseMarkdown(string(body))
-		bytes, _ := json.Marshal(year)
-		println(string(bytes))
+		bytes, _ := json.MarshalIndent(year, "", "  ")
+		fmt.Println(string(bytes))
 	}
 }
